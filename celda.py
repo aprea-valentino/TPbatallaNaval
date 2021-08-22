c4a766709f32c5f4e4a50ea8096ffb6d0f196ef9
valentino <vaprea@alumno.huergo.edu.ar>
2021-08-22 13:56:30 -0300

    agrego los arreglos de celda y tablero

--- celda.py
+++ celda.py
@@ -1,17 +1,10 @@
+from Barco import Barco
 class Celda:
     def __init__(self, estado, barco):
         self.estado = estado
-        self.barco = barco
-        self.cont = 0       
+        self.barco = barco       
         
-    def agregarBarco(self, estado, barco):
-        self.estado = True
-        self.barco = False
-
-    def sacarBarco(self, estado, barco):
-        self.estado = False
-        self.barco = None
-
-    def barcoHundido(self, estado, barco):
-        self.estado = True
-        self.barco = True
+    def cambiarCelda(self, estado, barco):
+        self.estado = estado
+        self.barco = barco
+        
--- tablero.py
+++ tablero.py
@@ -1,8 +1,21 @@
 import random
+from Celda import Celda
+
 class Tablero:
     def __init__(self):
         self.lista= []
+        self.celda = Celda(False, None)
+        self.crearCeldas()
         
+    def agregarBarco(self, posX, posY):
+        self.lista[posX][posY].cambiarCelda(True, False)
+        
+    def sacarBarco(self, posX, posY):
+        self.lista[posX][posY].cambiarCelda(False, None)
+
+    def barcoHundido(self, posX, posY):
+        self.lista[posX][posY].cambiarCelda(True, True)
+           
     def crearCeldas(self):
         self.lista= []
         for i in range(0, 8):
@@ -18,23 +31,26 @@ class Tablero:
                 while self.lista[posX][posY].estado == True:
                     posX = int (random.randrange(0, 8))
                     posY = int (random.randrange(0, 8))
-                self.lista[posX][posY].agregarBarco(True, Barco(False))               
+                self.agregarBarco(posX, posY)               
             else:
-                self.lista[posX][posY].agregarBarco(True, Barco(False))
+                self.agregarBarco(posX, posY)
                 
                 
     def ubicaciones(self):
         letras = ['A','B','C','D','E','F','G','H']
         for i in range(0, 8):
             for x in range(0, 8):
-                if self.lista[i][x].estado == True:
-                    print("Hay un barco en: " + str(letras[i]) + str(x + 1))
+                if self.lista[i][x].estado == True and self.lista[i][x].barco == False:
+                   print("Hay un barco en: " + str(letras[i]) + str(x + 1))
+                elif self.lista[i][x].estado == True and self.lista[i][x].barco == True:
+                    print("Hay un barco hundido en: " + str(letras[i]) + str(x + 1))
+                    
                     
     def borrarBarcos(self):
         for i in range(0, 8):
             for x in range(0, 8):
                 if self.lista[i][x].estado == True:
-                    self.lista[i][x].sacarBarco(False, Barco(None))
+                    self.sacarBarco(i, x)
                     
                 
     def dispararTorpedo(self, posX, posY):
@@ -42,6 +58,8 @@ class Tablero:
             print("Ya dejalo ya esta muerto")
         elif self.lista[posX-1][posY-1].barco == False:
             print("le pegaste y lo hundiste, pobre barco")
-            self.lista[posX-1][posY-1].barcoHundido(True, Barco(True))
+            self.barcoHundido(posX-1, posY-1)
         elif self.lista[posX-1][posY-1].barco == None:
             print("water")
+            
+
