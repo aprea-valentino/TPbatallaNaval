960ff44606e461418f8e7e4d07cdda5b08151f04
valentino <vaprea@alumno.huergo.edu.ar>
2021-08-25 12:29:13 -0300

    agrego que se pueda jugar con un bot pero sin clase TableroBot

--- Juego.py
+++ Juego.py
@@ -1,9 +1,9 @@
 import time
+import random
 from Tablero import Tablero
 class Juego:
     def __init__(self):
         self.tablero = Tablero()
-        self.tablero.crearCeldas()
         self.inicio()
 
     def inicio(self):
@@ -18,7 +18,7 @@ class Juego:
             self.barcosRandom()
             self.decision()
         if opcion == 2:
-            self.agregarBarco()
+            self.agregarBarcos()
         if opcion == 3:
             exit()
             
@@ -28,21 +28,32 @@ class Juego:
         print("Que wishea hacer")
         print("Disparar(1)")
         print("Ver ubicaciones de los barcos(2)")
-        print("Borrar y cu8enta nueva(3)")
+        print("Agregar un barco(3)")
+        print("Eliminar un barco(4)")
+        print("Borrar y cuenta nueva(5)")
         opcion= int(input("ingrese decision: "))
         if opcion==1:
             self.disparar()
-        if opcion==2:
+        elif opcion==2:
             self.verUbicaciones()
-        if opcion==3:
+        elif opcion==5:
             self.borrarBarcos()
+        elif opcion==4:
+            self.sacarBarco()
+        elif opcion == 3:
+            self.agregarUnBarco()
+        elif opcion==12:
+            self.tablero.ubicacionesBot()
+            self.decision()
             
     def barcosRandom(self):
         self.tablero.barcosRandom()
+        self.tablero.barcosRandomBot()
         
     def verUbicaciones(self):
         print("------------------------------------------------------------")
         self.tablero.ubicaciones()
+        time.sleep(1)
         self.decision()
         
     def disparar(self):
@@ -50,18 +61,47 @@ class Juego:
         posX = int(input("donde quiere disparar en x: "))
         posY = int(input("ahora en y: "))
         self.tablero.dispararTorpedo(posX, posY)
-        self.decision()
+        if self.tablero.cont == 2:
+            self.borrarBarcos()
+            self.inicio()
+        else:
+            print("Turno del bot")
+            time.sleep(1)
+            self.disparaBot()
+            self.decision()
+            
+    def disparaBot(self):
+        posX = int(random.randrange(0, 8))
+        posY = int(random.randrange(0, 8))
+        self.tablero.recibirTorpedo(posX, posY)
+        print(posX)
+        print(posY)
         
     def borrarBarcos(self):
         self.tablero.borrarBarcos()
+        self.tablero.borrarBarcosBot()
         self.inicio()
 
-    def agregarBarco(self):
+    def sacarBarco(self):
+        print("------------------------------------------------------------")
+        posX = int(input("seleccione una posicion en x: "))
+        posY = int(input("seleccione una posicion en y: "))
+        self.tablero.sacarBarco(posX - 1, posY - 1)
+        self.decision()
+
+    def agregarUnBarco(self):
+        posX = int(input("seleccione una posicion en x: "))
+        posY = int(input("seleccione una posicion en y: "))
+        self.tablero.agregarBarco(posX-1, posY-1)
+        self.decision()
+
+    def agregarBarcos(self):
         for i in range(0,8):
             print("------------------------------------------------------------")
             posX = int(input("seleccione una posicion en x: "))
             posY = int(input("seleccion una posiscion en y: "))
             self.tablero.agregarBarco(posX-1, posY-1)
+        time.sleep(1)
         self.decision()
 
 if __name__=="__main__":
--- tablero.py
+++ tablero.py
@@ -4,17 +4,30 @@ from Celda import Celda
 class Tablero:
     def __init__(self):
         self.lista= []
+        self.listaBot = []
         self.celda = Celda(False, None)
         self.crearCeldas()
+        self.crearCeldasBot()
+        self.cont = 0
+        self.contBot = 0
         
     def agregarBarco(self, posX, posY):
         self.lista[posX][posY].cambiarCelda(True, False)
         
+    def agregarBarcoBot(self, posX, posY):
+        self.listaBot[posX][posY].cambiarCelda(True, False)
+        
     def sacarBarco(self, posX, posY):
         self.lista[posX][posY].cambiarCelda(False, None)
+        
+    def sacarBarcoBot(self, posX, posY):
+        self.listaBot[posX][posY].cambiarCelda(False, None)
 
     def barcoHundido(self, posX, posY):
         self.lista[posX][posY].cambiarCelda(True, True)
+        
+    def barcoHundidoBot(self, posX, posY):
+        self.listaBot[posX][posY].cambiarCelda(True, True)
            
     def crearCeldas(self):
         self.lista= []
@@ -22,6 +35,14 @@ class Tablero:
             self.lista.append([])
             for x in range (0, 8):
                 self.lista[i].append(Celda(False, None))
+                
+    def crearCeldasBot(self):
+        self.listaBot= []
+        for i in range(0, 8):
+            self.listaBot.append([])
+            for x in range (0, 8):
+                self.listaBot[i].append(Celda(False, None))
+                
                 
     def barcosRandom(self):
         for i in range(0, 8):
@@ -35,6 +56,18 @@ class Tablero:
             else:
                 self.agregarBarco(posX, posY)
                 
+    def barcosRandomBot(self):
+        for i in range(0, 8):
+            posX = int (random.randrange(0, 8))
+            posY = int (random.randrange(0, 8))
+            if self.listaBot[posX][posY].estado == True:
+                while self.listaBot[posX][posY].estado == True:
+                    posX = int (random.randrange(0, 8))
+                    posY = int (random.randrange(0, 8))
+                self.agregarBarcoBot(posX, posY)               
+            else:
+                self.agregarBarcoBot(posX, posY)
+                
                 
     def ubicaciones(self):
         letras = ['A','B','C','D','E','F','G','H']
@@ -45,6 +78,14 @@ class Tablero:
                 elif self.lista[i][x].estado == True and self.lista[i][x].barco == True:
                     print("Hay un barco hundido en: " + str(letras[i]) + str(x + 1))
                     
+    def ubicacionesBot(self):
+        letras = ['A','B','C','D','E','F','G','H']
+        for i in range(0, 8):
+            for x in range(0, 8):
+                if self.listaBot[i][x].estado == True and self.listaBot[i][x].barco == False:
+                   print("Hay un barco en: " + str(letras[i]) + str(x + 1))
+                elif self.listaBot[i][x].estado == True and self.listaBot[i][x].barco == True:
+                    print("Hay un barco hundido en: " + str(letras[i]) + str(x + 1))
                     
     def borrarBarcos(self):
         for i in range(0, 8):
@@ -52,14 +93,42 @@ class Tablero:
                 if self.lista[i][x].estado == True:
                     self.sacarBarco(i, x)
                     
-                
-    def dispararTorpedo(self, posX, posY):
+    def borrarBarcosBot(self):
+        for i in range(0, 8):
+            for x in range(0, 8):
+                if self.listaBot[i][x].estado == True:
+                    self.sacarBarcoBot(i, x)
+                    
+    def recibirTorpedo(self, posX, posY):
         if self.lista[posX-1][posY-1].barco == True:
-            print("Ya dejalo ya esta muerto")
-        elif self.lista[posX-1][posY-1].barco == False:
-            print("le pegaste y lo hundiste, pobre barco")
+            print("El bot le pego devuelta al mismo")
+
+        if self.lista[posX-1][posY-1].barco == False:
+            print("Te han hundido un barco")
+            self.contBot = self.comtBot + 1
             self.barcoHundido(posX-1, posY-1)
-        elif self.lista[posX-1][posY-1].barco == None:
+            if self.contBot == 8:
+                print("PERDISTE CONTRA UN BOT")
+                pri
+            
+        if self.lista[posX-1][posY-1].barco == None:
+            print("No has recibido ningun daño")
+                    
+    def dispararTorpedo(self, posX, posY):
+        if self.listaBot[posX-1][posY-1].barco == True:
+            print("Ya dejalo ya esta muerto")
+            
+        if self.listaBot[posX-1][posY-1].barco == False:
+            print("Le pegaste y lo hundiste, pobre barco")
+            self.cont = self.cont + 1
+            self.barcoHundidoBot(posX-1, posY-1)
+            
+            if self.cont == 2:
+                print("GANASTE")
+                print("Reiniciando el juego...")
+                
+            
+        if self.listaBot[posX-1][posY-1].barco == None:
             print("water")
             
 
